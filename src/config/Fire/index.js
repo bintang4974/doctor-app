13597fcdd5a696e7e45ea573646e8456a5efa4be
bintang4974 <bintangramadhan4974@gmail.com>
2021-06-05 12:54:52 +0700

    first commit

---
+++ src/config/Fire/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBMJGIjo9SAqo8t3A9T8pDOsFbLXqowO4k",
+  authDomain: "my-doctor-01-38d89.firebaseapp.com",
+  projectId: "my-doctor-01-38d89",
+  storageBucket: "my-doctor-01-38d89.appspot.com",
+  messagingSenderId: "453977740422",
+  appId: "1:453977740422:web:3b2e7c8cfcd1dccc986d07"
+});
+
+const Fire = firebase;
+
+export default Fire;
